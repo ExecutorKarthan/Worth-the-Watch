c87706172557daf4aa242bf1c928e9fbdda2214d
Jared Woodcock <jared.woodcock@yahoo.com>
2024-01-18 22:02:26 -0600

    Styling of dashboard and review creation

--- public/js/reviews.js
+++ public/js/reviews.js
@@ -1,11 +1,12 @@
 const reviewCreate = async (event) => {
     event.preventDefault();
-    const title = document.querySelector('#title-create').value.trim();
+    console.log('Button Clicked');
+    const title = document.querySelector('#title-create').innerText.trim();
+    const overview = document.querySelector('#overview').innerText.trim();
+    const releaseDate = document.querySelector('#release-date').innerText.trim()
     const body = document.querySelector('#review-create').value.trim();
-    const overview = document.querySelector('#overview').value.trim();
-    const releaseDate = document.querySelector('#release-date').value.trim();
 
-    console.log(title, body, overview, releaseDate)
+    console.log(title, overview, releaseDate, body)
 
     if(title && body && overview && releaseDate) {
         const response = await fetch('/api/review/create-review', {
